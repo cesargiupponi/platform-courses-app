ae7014def9e8f7135b892c4788a3ac902a1cb3b2
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-02-10 20:38:20 -0300

    Create project

---
+++ PlatformCoursesApp/PlatformCoursesApp/PlatformCoursesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatformCoursesAppApp.swift
+//  PlatformCoursesApp
+//
+//  Created by Cesar Giupponi on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PlatformCoursesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
