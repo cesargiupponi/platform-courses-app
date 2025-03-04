cca5f6259acd7fc1e5db1d6dc977728581fb708b
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-03-04 11:54:40 -0300

    Add dismiss and drag anchors

--- PlatformCoursesApp/PlatformCoursesApp/ContentView.swift
+++ PlatformCoursesApp/PlatformCoursesApp/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
     @State var showCard = false
+    @State var bottomState: CGSize = .zero
+    @State var showFull = false
 
     var body: some View {
         ZStack {
@@ -107,8 +109,35 @@ struct ContentView: View {
 
             BottomCardView()
                 .offset(x: 0, y: showCard ? 360 : 1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
-                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                .animation(
+                    .timingCurve(0.2, 0.8, 0.2, 1,
+                                 duration: 0.8)
+                    )
+                .gesture(
+                    DragGesture().onChanged { value in
+                        bottomState = value.translation
+                        if showFull {
+                            bottomState.height += -280
+                        }
+                        if bottomState.height < -280 {
+                            bottomState.height = -280
+                        }
+                    }
+                    .onEnded { value in
+                        if bottomState.height > 50 {
+                            showCard = false
+                        }
+                        if (bottomState.height < -100 && !showFull) || (bottomState.height < -230 && showFull) {
+                            bottomState.height = -280
+                            showFull = true
+                        } else {
+                            bottomState = .zero
+                            showFull = false
+                        }
+                    }
+                )
         }
     }
 }
